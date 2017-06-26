2b6edec81403e72ee88f9a8188ce5b078d80d835
VikaNoumenko <vika.noumenko@gmail.com>
2017-06-26 22:54:21 +0400

    Spring Security

--- src/main/java/ru/itis/config/PersistenceConfig.java
+++ src/main/java/ru/itis/config/PersistenceConfig.java
@@ -1,76 +1,52 @@
 package ru.itis.config;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.env.Environment;
-import org.springframework.dao.annotation.PersistenceExceptionTranslationPostProcessor;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.orm.jpa.JpaTransactionManager;
-import org.springframework.orm.jpa.JpaVendorAdapter;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
-import javax.persistence.EntityManagerFactory;
 import javax.sql.DataSource;
-import java.util.Properties;
 
 @Configuration
+@EnableJpaRepositories("ru.itis.dao")
 @EnableTransactionManagement
 public class PersistenceConfig {
-    @Autowired
-    private Environment environment;
-
     @Bean
-    public DataSource dataSource() {
-        DriverManagerDataSource dataSource = new DriverManagerDataSource();
-        dataSource.setUrl(environment.getProperty("jdbc.url"));
-        dataSource.setUsername(environment.getProperty("jdbc.name"));
-        dataSource.setPassword(environment.getProperty("jdbc.password"));
-        dataSource.setDriverClassName(environment.getProperty("jdbc.driver"));
-        return dataSource;
+    PlatformTransactionManager transactionManager() {
+        return new JpaTransactionManager(entityManagerFactory().getObject());
     }
 
-    // менеджер транзакций
     @Bean
-    public PlatformTransactionManager transactionManager(EntityManagerFactory entityManagerFactory){
-        JpaTransactionManager transactionManager = new JpaTransactionManager();
-        transactionManager.setEntityManagerFactory(entityManagerFactory);
+    LocalContainerEntityManagerFactoryBean entityManagerFactory() {
+        LocalContainerEntityManagerFactoryBean localContainerEntityManagerFactoryBean =
+                new LocalContainerEntityManagerFactoryBean();
 
-        return transactionManager;
+        localContainerEntityManagerFactoryBean.setDataSource(dataSource());
+        localContainerEntityManagerFactoryBean.setJpaVendorAdapter(hibernateJpaVendorAdapter());
+        localContainerEntityManagerFactoryBean.setPackagesToScan("ru.itis.model");
+        return localContainerEntityManagerFactoryBean;
     }
 
-    // контейнер JPA
     @Bean
-    public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
-        LocalContainerEntityManagerFactoryBean entityManager = new LocalContainerEntityManagerFactoryBean();
-        entityManager.setDataSource(dataSource());
-        entityManager.setPackagesToScan("ru.itis.models");
-
-        // прослойка между Jpa и hibernate
-        JpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
-        entityManager.setJpaVendorAdapter(vendorAdapter);
-        entityManager.setJpaProperties(additionalProperties());
-
-        return entityManager;
+    HibernateJpaVendorAdapter hibernateJpaVendorAdapter() {
+        HibernateJpaVendorAdapter adapter = new HibernateJpaVendorAdapter();
+        adapter.setShowSql(true);
+        return adapter;
     }
 
-    // комонент отлова ошибок
-    @Bean
-    public PersistenceExceptionTranslationPostProcessor exceptionTranslation(){
-        return new PersistenceExceptionTranslationPostProcessor();
-    }
 
-    // настройки хибернейта
     @Bean
-    Properties additionalProperties() {
-        Properties properties = new Properties();
-        properties.setProperty("hibernate.hbm2ddl.auto", "update");
-        properties.setProperty("hibernate.dialect", "org.hibernate.dialect.PostgreSQL82Dialect");
-        properties.setProperty("hibernate.enable_lazy_load_no_trans", "true");
-        properties.setProperty("hibernate.show_sql", "true");
-        return properties;
+    DataSource dataSource() {
+        DriverManagerDataSource driverManagerDataSource = new DriverManagerDataSource();
+        driverManagerDataSource.setDriverClassName("org.postgresql.Driver");
+        driverManagerDataSource.setUrl("jdbc:postgresql://localhost:5432/itis_users");
+        driverManagerDataSource.setPassword("qwerty007");
+        driverManagerDataSource.setUsername("postgres");
+        return driverManagerDataSource;
     }
 }
